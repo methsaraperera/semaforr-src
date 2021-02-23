8fabe5fe786b081a8550d1a18fe3ea20099b1fd6
Raj Korpan <raj.m.korpan@gmail.com>
2021-02-22 22:58:05 -0500

    Met testing setting

--- launchfile.py
+++ launchfile.py
@@ -201,15 +201,15 @@ advisors = "/config/advisors2.conf"
 params = "/config/params4.conf"
 map_name = "met-1"
 for i in range(0,num_runs):
-    for j in range(1,2):
+    for j in range(1,6):
         why_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_explanations.txt"
         whyplan_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_explanations.txt"
         why_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_log.txt"
         whyplan_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_log.txt"
         situation_log_name = map_name + "_" + str(j) + "_" + str(i) + "_situation_log.txt"
-        log_name = map_name + "_mettest_" + str(j) + "_" + str(i) + ".txt"
-        # target_file_name = "target40test-" + str(j) + ".conf"
-        target_file_name = "target.conf"
+        log_name = map_name + "_test_" + str(j) + "_" + str(i) + ".txt"
+        target_file_name = "target40test-" + str(j) + ".conf"
+        # target_file_name = "target.conf"
         experiment(map_name, log_name, density, flow, risk, cusum, discount, explore, advisors, params, situations, spatials)
 
 # num_runs = 1
