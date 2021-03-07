76b447ed7fba68711d8a745ea647204f00bb8855
Raj Korpan <raj.m.korpan@gmail.com>
2021-03-07 17:18:02 -0500

    Setting for longer sequence

--- launchfile.py
+++ launchfile.py
@@ -55,17 +55,17 @@ def experiment(map_name, log_name, density, flow, risk, cusum, discount, explore
     log_file = open(log_name,"w")
     log_process = subprocess.Popen(['rostopic','echo','/decision_log'],stdout=log_file)
 
-    why_explanations_file = open(why_explanations_name,"w")
-    why_explanations_process = subprocess.Popen(['rostopic','echo','/explanations'],stdout=why_explanations_file)
+    # why_explanations_file = open(why_explanations_name,"w")
+    # why_explanations_process = subprocess.Popen(['rostopic','echo','/explanations'],stdout=why_explanations_file)
 
-    whyplan_explanations_file = open(whyplan_explanations_name,"w")
-    whyplan_explanations_process = subprocess.Popen(['rostopic','echo','/plan_explanations'],stdout=whyplan_explanations_file)
+    # whyplan_explanations_file = open(whyplan_explanations_name,"w")
+    # whyplan_explanations_process = subprocess.Popen(['rostopic','echo','/plan_explanations'],stdout=whyplan_explanations_file)
 
-    why_log_file = open(why_log_name,"w")
-    why_log_process = subprocess.Popen(['rostopic','echo','/explanations_log'],stdout=why_log_file)
+    # why_log_file = open(why_log_name,"w")
+    # why_log_process = subprocess.Popen(['rostopic','echo','/explanations_log'],stdout=why_log_file)
 
-    whyplan_log_file = open(whyplan_log_name,"w")
-    whyplan_log_process = subprocess.Popen(['rostopic','echo','/plan_explanations_log'],stdout=whyplan_log_file)
+    # whyplan_log_file = open(whyplan_log_name,"w")
+    # whyplan_log_process = subprocess.Popen(['rostopic','echo','/plan_explanations_log'],stdout=whyplan_log_file)
 
     # start semaforr
     semaforr_process = subprocess.Popen(['rosrun','semaforr','semaforr', semaforr_path, target_set, map_config, map_dimensions, advisors, params, situations, spatials])
@@ -77,11 +77,11 @@ def experiment(map_name, log_name, density, flow, risk, cusum, discount, explore
     # print "waiting,,"
 
     # start why
-    why_process = subprocess.Popen(['rosrun','why','why'])
+    # why_process = subprocess.Popen(['rosrun','why','why'])
     # print "waiting,,"
 
     # start why_plan
-    why_plan_process = subprocess.Popen(['rosrun','why_plan','why_plan'])
+    # why_plan_process = subprocess.Popen(['rosrun','why_plan','why_plan'])
     # print "waiting,,"
    
     rviz_process = subprocess.Popen(['rosrun','rviz','rviz'])
@@ -121,19 +121,19 @@ def experiment(map_name, log_name, density, flow, risk, cusum, discount, explore
     #crowd_process.terminate()
     if map_name != "gradcenter-5":
         situation_process.terminate()
-    why_process.terminate()
-    why_plan_process.terminate()
+    # why_process.terminate()
+    # why_plan_process.terminate()
     # print "Why terminated!"
     log_process.terminate()
     log_file.close()
-    why_explanations_process.terminate()
-    why_explanations_file.close()
-    whyplan_explanations_process.terminate()
-    whyplan_explanations_file.close()
-    why_log_process.terminate()
-    why_log_file.close()
-    whyplan_log_process.terminate()
-    whyplan_log_file.close()
+    # why_explanations_process.terminate()
+    # why_explanations_file.close()
+    # whyplan_explanations_process.terminate()
+    # whyplan_explanations_file.close()
+    # why_log_process.terminate()
+    # why_log_file.close()
+    # whyplan_log_process.terminate()
+    # whyplan_log_file.close()
     # situation_log_process.terminate()
     # situation_log_file.close()
     time.sleep(1)
@@ -153,20 +153,20 @@ explore = "off"
 situations = "/config/situations5.conf"
 spatials = "/config/spatial_model.conf"
 
-num_runs = 7
-advisors = "/config/advisors2.conf"
-params = "/config/params5.conf"
-map_name = "gradcenter-5"
-for i in range(3,num_runs):
-    for j in range(0,1):
-        why_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_explanations.txt"
-        whyplan_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_explanations.txt"
-        why_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_log.txt"
-        whyplan_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_log.txt"
-        situation_log_name = map_name + "_" + str(j) + "_" + str(i) + "_situation_log.txt"
-        log_name = map_name + "_T1_CS_SM_PP_RPF_EX30_LLE_40RT_" + str(j) + "_" + str(i) + ".txt"
-        target_file_name = "target4-2.conf"
-        experiment(map_name, log_name, density, flow, risk, cusum, discount, explore, advisors, params, situations, spatials)
+# num_runs = 7
+# advisors = "/config/advisors2.conf"
+# params = "/config/params5.conf"
+# map_name = "gradcenter-5"
+# for i in range(3,num_runs):
+#     for j in range(0,1):
+#         why_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_explanations.txt"
+#         whyplan_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_explanations.txt"
+#         why_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_log.txt"
+#         whyplan_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_log.txt"
+#         situation_log_name = map_name + "_" + str(j) + "_" + str(i) + "_situation_log.txt"
+#         log_name = map_name + "_T1_CS_SM_PP_RPF_EX30_LLE_40RT_" + str(j) + "_" + str(i) + ".txt"
+#         target_file_name = "target4-2.conf"
+#         experiment(map_name, log_name, density, flow, risk, cusum, discount, explore, advisors, params, situations, spatials)
 
 # num_runs = 5
 # advisors = "/config/advisors2.conf"
@@ -198,21 +198,21 @@ for i in range(3,num_runs):
         target_file_name = "target100-2.conf"
         experiment(map_name, log_name, density, flow, risk, cusum, discount, explore, advisors, params, situations, spatials)
 
-# num_runs = 1
-# advisors = "/config/advisors2.conf"
-# params = "/config/params4.conf"
-# map_name = "met-1"
-# for i in range(0,num_runs):
-#     for j in range(1,6):
-#         why_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_explanations.txt"
-#         whyplan_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_explanations.txt"
-#         why_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_log.txt"
-#         whyplan_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_log.txt"
-#         situation_log_name = map_name + "_" + str(j) + "_" + str(i) + "_situation_log.txt"
-#         log_name = map_name + "_test_EX120_3000_" + str(j) + "_" + str(i) + ".txt"
-#         target_file_name = "target40test-" + str(j) + ".conf"
-#         # target_file_name = "target.conf"
-#         experiment(map_name, log_name, density, flow, risk, cusum, discount, explore, advisors, params, situations, spatials)
+num_runs = 2
+advisors = "/config/advisors2.conf"
+params = "/config/params4.conf"
+map_name = "met-1"
+for i in range(1,num_runs):
+    for j in range(1,6):
+        why_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_explanations.txt"
+        whyplan_explanations_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_explanations.txt"
+        why_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_log.txt"
+        whyplan_log_name = map_name + "_" + str(j) + "_" + str(i) + "_why_plan_log.txt"
+        situation_log_name = map_name + "_" + str(j) + "_" + str(i) + "_situation_log.txt"
+        log_name = map_name + "_test_EX120_3000_" + str(j) + "_" + str(i) + ".txt"
+        target_file_name = "target40test-" + str(j) + ".conf"
+        # target_file_name = "target.conf"
+        experiment(map_name, log_name, density, flow, risk, cusum, discount, explore, advisors, params, situations, spatials)
 
 # num_runs = 1
 # advisors = "/config/advisors2.conf"
